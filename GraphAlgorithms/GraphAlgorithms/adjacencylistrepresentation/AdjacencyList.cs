fc6536391c6bd98990909db544da2d3313be22a7
Vipin K <vipin.saini17@gmail.com>
2020-05-30 11:45:38 -0400

    vipin.saini17@gmail.com GraphAlgorithms data structure made life easy

---
+++ GraphAlgorithms/GraphAlgorithms/adjacencylistrepresentation/AdjacencyList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphAlgorithms.adjacencylistrepresentation
+{
+    /// <summary>
+    /// There are two ways to represents the graph
+    /// 1. Adjacency List - You can use List, ArrayList or LinkedList to represents this
+    /// 2. Adjacency Matrix - Can be achieve using two dimensional array 
+    /// </summary>
+    public class AdjacencyList
+    {
+        static int N = 5;
+
+        static LinkedList<int>[] adj = new LinkedList<int>[N];
+        public static void GenerateAdjList()
+        {
+            for (int i = 0; i < N; i++)
+                adj[i] = new LinkedList<int>();
+
+            addEdge(adj, 0, 1);
+            addEdge(adj, 0, 4);
+            addEdge(adj, 1, 2);
+            addEdge(adj, 1, 3);
+            addEdge(adj, 1, 4);
+            addEdge(adj, 2, 3);
+            addEdge(adj, 3, 4);
+            printGraph(adj);
+
+
+            bool[] visited = new bool[N];
+
+        }
+
+        public static void printGraph(LinkedList<int>[] adj)
+        {
+            for (int i = 0; i < adj.Length; i++)
+            {
+                Console.WriteLine("\nAdjacency list of vertex " + i);
+                Console.Write("head");
+
+                foreach (var item in adj[i])
+                {
+                    Console.Write(" -> " + item);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public static void addEdge(LinkedList<int>[] adj, int u, int v)
+        {
+            Console.WriteLine("added {0}, {1}", u, v);
+            adj[u].AddLast(v);
+            adj[v].AddLast(u);
+        }
+    }
+}
