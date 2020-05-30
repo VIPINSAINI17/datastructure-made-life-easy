abee134f66f37f80ed16a0ec1231a6f3f8d831b4
Vipin K <vipin.saini17@gmail.com>
2020-05-30 11:26:57 -0400

    vipin.saini17@gmail.com GraphAlgorithms data structure made life easy

---
+++ GraphAlgorithms/GraphAlgorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿using GraphAlgorithms.graphadjacencylist.dfs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GraphDfs.GenerateAdjList();
+        }
+    }
+}
