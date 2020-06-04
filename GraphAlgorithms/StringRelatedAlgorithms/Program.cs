6c6f00a9798d2684db6391c15d43028423248a71
Vipin K <vipin.saini17@gmail.com>
2020-06-04 19:38:40 -0400

    StringAlgorithms data structure made life StringRelatedAlgorithms

---
+++ GraphAlgorithms/StringRelatedAlgorithms/Program.cs
@@ -0,0 +1,21 @@
+﻿using StringRelatedAlgorithms.longtestcommonsubsequence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringRelatedAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 ="elephant";
+            string s2 ="eretpat";
+            int len = LongestCommonSubsequence.FindLongestCommonSubsequnec(s1, s2, 0, 0);
+
+            Console.WriteLine(len);
+        }
+    }
+}
---
+++ GraphAlgorithms/StringRelatedAlgorithms/longtestcommonsubsequence/LongestCommonSubsequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringRelatedAlgorithms.longtestcommonsubsequence
+{
+    public class LongestCommonSubsequence
+    {
+
+        public static int FindLongestCommonSubsequnec(string s1, string s2, int i1, int i2)
+        {
+            if (i1 == s1.Length || i2 == s2.Length) return 0;
+
+            int c3 = 0;
+            if (s1[i1] == s2[i2])
+                c3 = 1 + FindLongestCommonSubsequnec(s1, s2, i1 + 1, i2 + 1);
+
+            int c1 = FindLongestCommonSubsequnec(s1, s2, i1 + 1, i2);
+
+            int c2 = FindLongestCommonSubsequnec(s1, s2, i1, i2+1);
+
+            int longestSusequence = Math.Max(c3, Math.Max(c1,c2));
+
+            return longestSusequence;
+        }
+    }
+}
