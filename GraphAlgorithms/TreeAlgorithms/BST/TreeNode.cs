627605988dd2a52506478be4d65578671d48fc7e
Vipin K <vipin.saini17@gmail.com>
2020-06-09 14:27:53 -0400

    Tree algorithms

---
+++ GraphAlgorithms/TreeAlgorithms/BST/TreeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeAlgorithms.BST
+{
+    public class TreeNode
+    {
+        public int Data { get; set; }
+        public TreeNode Right { get; set; }
+        public TreeNode Left { get; set; }
+
+        public TreeNode(int data)
+        {
+            this.Data = data;
+        }
+    }
+}
--- GraphAlgorithms/TreeAlgorithms/BST/insert/InsertInTree.cs
+++ GraphAlgorithms/TreeAlgorithms/BST/insert/InsertInTree.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TreeAlgorithms.BST;
 
 namespace TreeAlgorithms.BST.insert
 {
-    class InsertInTree
+    public class InsertInTree
     {
+        /// <summary>
+        /// This will insert a new node for a tree in recursive way.
+        /// TODO - Time Complexity
+        /// TODO - Space Complexity 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static TreeNode InsertIntoTree(TreeNode root, int data) {
+
+            //TODO - Check first if this is the first node to insert which will be the root
+            if (root == null)
+            {
+                TreeNode rootNode = new TreeNode(data);
+                return rootNode;
+            }
+
+            // TODO -if root node is already setup means either this insertion will be on left or right
+            // TODO insert on left if value is less than root node
+            // TODO insert on right if value is greater than root node 
+
+            if (root.Data < data) // Right
+                InsertIntoTree(root.Right, data);
+            if(root.Data > data) // Left
+                InsertIntoTree(root.Left, data);
+
+            /* return the (unchanged) node pointer */
+            return root;
+        }
     }
 }
