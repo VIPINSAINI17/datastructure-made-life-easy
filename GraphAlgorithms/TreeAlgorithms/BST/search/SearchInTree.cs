19f2e9d0f7828c5c57910272ffae1a3e41b33713
Vipin K <vipin.saini17@gmail.com>
2020-06-11 17:49:10 -0400

    Tree algorithms

--- GraphAlgorithms/TreeAlgorithms/BST/search/SearchInTree.cs
+++ GraphAlgorithms/TreeAlgorithms/BST/search/SearchInTree.cs
@@ -6,8 +6,38 @@ using System.Threading.Tasks;
 
 namespace TreeAlgorithms.BST.search
 {
+    //    Binary Search Tree Delete
+    // When we delete a node, three possibilities arise.
+    //1) Find a value in the below given tree
+
+    //              50                           
+    //           /     \         
+    //          30      70      
+    //         /  \    /  \        
+    //       20   40  60   80            
+
     public class SearchInTree
     {
-
+        public static TreeNode SearchForValue(TreeNode node, int value)
+        {
+            if (node == null) // BASECASE-1 : Check for the incoming node is null
+            {
+                Console.WriteLine("Value: " + value + " not found in BST.");
+                return null;
+            }
+            else if (node.Data == value) // BASECASE-2 - Check if incoming  
+            {
+                Console.WriteLine("Value: " + value + " found in BST.");
+                return node;
+            }
+            else if (value < node.Data)  // If find value is less than current value, can be find in left recusively 
+            {
+                return SearchForValue(node.Left, value);
+            }
+            else // If find value is greater than current value, can be find in left recusively 
+            {
+                return SearchForValue(node.Right, value);
+            }
+        }// end of method
     }
 }
